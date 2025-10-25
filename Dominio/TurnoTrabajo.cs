236b062f7772789122432260bfc9dd0a149ef43e
gomezalejandromatias <mati.gorriti1@gmail.com>
2025-10-25 12:01:07 -0300

    pagina 1

---
+++ Dominio/TurnoTrabajo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class TurnoTrabajo
+    {
+         public int IdturnoTrabajo { get; set; }
+        public Medico Medico { get; set; }
+        public Especialidad Especialidad { get; set; }
+        public DateTime HoraInicio { get; set; }
+        public DateTime HoraFin{ get; set; }
+        public Consultorio Consultorio { get; set; }
+        public bool Activo { get; set; }    
+
+
+
+
+
+    }
+}
